c3f61107cc2e89769db64e647d9570bed58ab752
higer <higer2005@gmail.com>
2016-06-27 09:38:36 +1000

    add robotium testcase

---
+++ executorproject/output.java
@@ -0,0 +1,36 @@
+package com.tonyinfostorm.hellocommit.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.*;
+import com.tonyinfostorm.hellocommit.MainActivity;
+
+
+public class CheckHelloDisplay extends ActivityInstrumentationTestCase2<MainActivity> {
+  	private Solo solo;
+  	
+  	public CheckHelloDisplay() {
+		super(MainActivity.class);
+  	}
+
+  	public void setUp() throws Exception {
+        super.setUp();
+		solo = new Solo(getInstrumentation());
+		getActivity();
+  	}
+  
+   	@Override
+   	public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+  	}
+  
+	public void testRun() {
+        //Wait for activity: 'com.tonyinfostorm.hellocommit.MainActivity'
+		solo.waitForActivity(com.tonyinfostorm.hellocommit.MainActivity.class, 2000);
+        //Set default small timeout to 45158 milliseconds
+		Timeout.setSmallTimeout(45158);
+        //Assert that: 'Hello World!' is shown
+		assertTrue("'Hello World!' is not shown!", solo.waitForText(java.util.regex.Pattern.quote("Hello World!"), 1, 20000, true, true));
+	}
+}
