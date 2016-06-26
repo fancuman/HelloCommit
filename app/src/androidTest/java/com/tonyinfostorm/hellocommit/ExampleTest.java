14797d3b3850c2cda2d4e6ef6cbf06a40fcf0eac
higer <higer2005@gmail.com>
2016-06-26 21:21:11 +1000

    add wrong instrument test

---
+++ app/src/androidTest/java/com/tonyinfostorm/hellocommit/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.tonyinfostorm.hellocommit;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by higer on 2016/6/26.
+ */
+public class ExampleTest extends InstrumentationTestCase{
+    public void testCase1(){
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality+1);
+    }
+}
