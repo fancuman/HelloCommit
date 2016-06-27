f817de9b23ad906fc3bdfd74ee31395b3e359a88
higer <higer2005@gmail.com>
2016-06-27 11:50:55 +1000

    trigger Jenkins autobuild

--- app/src/androidTest/java/com/tonyinfostorm/hellocommit/ExampleTest.java
+++ app/src/androidTest/java/com/tonyinfostorm/hellocommit/ExampleTest.java
@@ -8,7 +8,7 @@ import android.test.InstrumentationTestCase;
 public class ExampleTest extends InstrumentationTestCase{
     public void testCase1(){
         final int expected = 1;
-        final int reality = 5;
+        final int reality = 1;
         assertEquals(expected, reality);
     }
 }
